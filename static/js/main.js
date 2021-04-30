a54560dc6f08ff4b96cf04a7d8759f0c9f99d84c
Danny Arvizu <darvizu061@gmail.com>
2021-04-29 23:49:53 -0500

    Added external js files

---
+++ static/js/main.js
@@ -0,0 +1,38 @@
+function toggleMenu() {
+  var x = document.getElementById("hiddenMenu");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function toggleStatus() {
+  var x = document.getElementById("statusSubcontainerOn");
+  var y = document.getElementById("statusSubcontainerOff");
+  
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    y.style.display = "block";
+    toggleRides();
+  } else {
+    x.style.display = "block";
+    y.style.display = "none";
+    toggleRides();
+  }
+}
+function toggleRides(){
+  var x = document.getElementsByClassName("rideRow");
+  var y = document.getElementById("offlineRidesContainer");
+
+  if (x[0].style.display === "block"){
+    y.style.display = "block";
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+  } else {
+    y.style.display = "none";
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "block";
+    }
+  }
+}
